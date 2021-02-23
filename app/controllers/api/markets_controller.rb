2dd63c99b00e74a46c96084ea11120c5315fa3bb
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-23 19:52:15 +0100

    Successfully renders dataset with market caps

--- app/controllers/api/markets_controller.rb
+++ app/controllers/api/markets_controller.rb
@@ -1,8 +1,14 @@
 class Api::MarketsController < ApplicationController
+  
   def index
-    nomics = Rails.application.credentials.nomics 
-    
-    response = RestClient.get("https://api.nomics.com/v1/market-cap/history?key=#{nomics}&start=#{params["date"]}")
-    render status: 200
+    nomics = Rails.application.credentials.nomics
+    response = RestClient.get("https://api.nomics.com/v1/market-cap/history?key=#{nomics}&start=#{params['date']}")
+    res_body = JSON.parse(response.body)
+    res_body.each do |day|
+      day['timestamp'] = day['timestamp'].to_datetime.strftime('%d/%m')
+    end
+    render json: {
+      market_data: res_body
+    }
   end
 end
--- spec/requests/api_can_get_market_cap_data_spec.rb
+++ spec/requests/api_can_get_market_cap_data_spec.rb
@@ -1,18 +1,22 @@
-RSpec.describe 'GET /api/markets',type: :request do
-
+RSpec.describe 'GET /api/markets', type: :request do
   describe 'successfully' do
-
     before do
       get '/api/markets',
-      params: {
-        date: '2021-02-16T14:13:31.364Z'
-      }
-      end
+          params: {
+            date: '2021-02-16T14:13:31.364Z'
+          }
+    end
 
-      it 'responds with a 200 status' do
-        expect(response). to have_http_status 200
-      end
+    it 'responds with a 200 status' do
+      expect(response).to have_http_status 200
+    end
 
-    
+    it 'responds with expected length of data list' do
+      expect(response_json['market_data'].count).to eq 7
+    end
+
+    it 'responds with expected date format' do
+      expect(response_json['market_data'].first['timestamp']).to eq '17/02'
+    end
   end
 end

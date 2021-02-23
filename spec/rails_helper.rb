0af0bcaba6f2698371c788db4f7230b1897075bc
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-23 12:23:40 +0100

    Installs and configures Rest-client and Webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,13 @@
 require 'coveralls'
 Coveralls.wear_merged!('rails')
-require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'spec_helper'
+require 'webmock/rspec'
+WebMock.disable_net_connect!
 
 begin
   ActiveRecord::Migration.maintain_test_schema!

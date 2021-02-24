791e924e029a20584d7b5dad1fca2bbbf99a5d73
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-24 19:41:52 +0100

    API recieves info successfully, filters, and renders a cleaner list

--- app/controllers/api/markets_controller.rb
+++ app/controllers/api/markets_controller.rb
@@ -1,19 +1,18 @@
 class Api::MarketsController < ApplicationController
   def index
     nomics = Rails.application.credentials.nomics
-    begin
-      response = RestClient.get("https://api.nomics.com/v1/market-cap/history?key=#{nomics}&start=#{params['date']}")
-      res_body = JSON.parse(response.body)
-      res_body.each do |day|
-        day['timestamp'] = day['timestamp'].to_datetime.strftime('%d/%m')
-      end
-      render json: {
-        market_data: res_body
-      }
-    rescue RestClient::UnprocessableEntity => e
-      render json: {
-        message: "External API couldn't handle your request"
-      }, status: 422
+    response = RestClient.get("https://api.nomics.com/v1/market-cap/history?key=#{nomics}&start=#{params['date']}")
+    res_body = JSON.parse(response.body)
+    res_body.each do |day|
+      day['timestamp'] = day['timestamp'].to_datetime.strftime('%d/%m')
     end
+
+    render json: {
+      market_data: res_body
+    }
+  rescue RestClient::UnprocessableEntity => e
+    render json: {
+      message: "External API couldn't handle your request"
+    }, status: 422
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 require 'webmock/rspec'
-WebMock.disable_net_connect!
+WebMock.enable_net_connect!
 
 begin
   ActiveRecord::Migration.maintain_test_schema!

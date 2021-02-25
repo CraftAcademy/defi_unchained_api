d026eb04d4b84f6353bd44ba4ac007874d1d7fc4
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-25 16:58:19 +0100

    Continued on tests setup

--- spec/requests/api_responds_with_news_feed_for_authenticated_users_spec.rb
+++ spec/requests/api_responds_with_news_feed_for_authenticated_users_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe 'GET /api/news', type: :request do
-  let!(:user) {create(:user)}
-  let(auth_headers) {user.create_new_auth_token}
+  let!(:user) { create(:user) }
+  let(auth_headers) { user.create_new_auth_token }
   describe 'successfully with authenticated user' do
     before do
       get '/api/news',
-      headers: auth_headers
+          headers: auth_headers
     end
 
     it 'returns a 200 status' do
@@ -16,23 +16,23 @@ RSpec.describe 'GET /api/news', type: :request do
     end
 
     it 'an article contains the expected title' do
-      expect(response_json['articles'].first).to eq "Is Blockchain the Future of Art Collecting?"
+      expect(response_json['articles'].first).to eq 'Is Blockchain the Future of Art Collecting?'
     end
 
     it 'an article contains the expected image url' do
-      expect(response_json['articles'].first).to eq "https://wwd.com/wp-content/uploads/2021/02/Installation-View37.jpg?w=640&h=415&crop=1"
+      expect(response_json['articles'].first).to eq 'https://wwd.com/wp-content/uploads/2021/02/Installation-View37.jpg?w=640&h=415&crop=1'
     end
 
     it 'an article contains the expected url' do
-      expect(response_json['articles'].first).to eq "https://wwd.com/eye/lifestyle/ry-david-bradley-the-hole-gallery-nyc-nft-blockchain-1234727774/"
+      expect(response_json['articles'].first).to eq 'https://wwd.com/eye/lifestyle/ry-david-bradley-the-hole-gallery-nyc-nft-blockchain-1234727774/'
     end
 
     it 'an article contains the expected description' do
-      expect(response_json['articles'].first).to eq "Artist Ry David Bradley is selling works from his new gallery show at The Hole in New York as physical art and nonfungible tokens through digital art marketplace SuperRare."
+      expect(response_json['articles'].first).to eq 'Artist Ry David Bradley is selling works from his new gallery show at The Hole in New York as physical art and nonfungible tokens through digital art marketplace SuperRare.'
     end
 
     it 'an article contains the expected date' do
-      expect(response_json['articles'].first).to eq "2021-02-25T12:30:42Z"
+      expect(response_json['articles'].first).to eq '2021-02-25T12:30:42Z'
     end
   end
 end
